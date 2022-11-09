b5ce9c730aa885be045ab1bcf6baf22a7463e458
javierTenorio0 <Javier.Tenorio@Fundacion-jala.org>
2022-11-08 21:03:54 -0600

    Clase 2

---
+++ dino_runner/components/game.py
@@ -0,0 +1,56 @@
+import pygame
+
+from dino_runner.components.dinosaur import Dinosour
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        pygame.display.set_icon(ICON)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.playing = False
+        self.game_speed = 20
+        self.x_pos_bg = 0
+        self.y_pos_bg = 380
+        self.player = Dinosour()
+        self.obstacle_manager = ObstacleManager()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.quit()
+    
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+
+    def update(self):
+        self.player.update()
+        self.obstacle_manager.update(self)
+
+
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255,255,255))
+        self.draw_background()
+        self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
+
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_background(self):
+        image_width = BG.get_width()
+        self.screen.blit(BG, (self.x_pos_bg,self.y_pos_bg))
+        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        if self.x_pos_bg <= image_width: 
+            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+            self.x_pos_bg = 0
+        self.x_pos_bg -= self.game_speed
---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,33 @@
+import pygame
+import random
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.utils.constants import (SMALL_CACTUS, LARGE_CACTUS)
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+    
+    def update(self, game):
+        if len(self.obstacles) == 0:
+            cactus_size = random.randint(0, 1)
+            if cactus_size == 0:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+                #for obstacle in self.obstacles:
+                    #obstacle.rect.y = 300
+            else:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+                for obstacle in self.obstacles:
+                    obstacle.rect.y = 325
+        
+        for obstacle in self.obstacles:
+            obstacle.update(self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(500)
+                game.playing = True
+            
+        for obstacle in self.obstacles:
+            obstacle.update(self.obstacles)
+
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen) 
